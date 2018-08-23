c79aae075085013457eaefda6a79cb1935e423ad
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-23 10:11:07 -0500

    adds pets.router.js

---
+++ server/routers/pets.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../modules/pool.js');
+
+//GET route
+
+//DELETE route
+
+//POST route
+
+//PUT route
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-//const _____Router= require('./routers/____.router.js)
+const petsRouter = require('./routers/pets.router.js');
 
 //Configure body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //Setup Routes
+app.use('/pets', petsRouter);
 
 //Static Files
 app.use(express.static('server/public'));
