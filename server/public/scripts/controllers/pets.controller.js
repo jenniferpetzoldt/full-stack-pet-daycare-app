c887802ecfc09e9ed57a9d039a27d4707964c0ac
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-23 11:47:15 -0500

    adds server and client side pets POST route

--- server/public/scripts/controllers/pets.controller.js
+++ server/public/scripts/controllers/pets.controller.js
@@ -1,6 +1,27 @@
 myApp.controller('PetsController', function($http){
     const vm = this;
     console.log('NG in pets');
+
+   function getPets(){
+       $http({
+           method: 'GET',
+       })
+   }
+   
+    //POST route to add pets to pets table in database
+    vm.addToPets = function(pet) {
+        console.log('in addToPets');
+        $http({
+            method: 'POST',
+            url: '/pets',
+            data: {name: name, color: color, breed: breed}
+        }).then(function(response){
+            console.log('/pets POST success:', response);
+        }).catch(function (error){
+            console.log('/pets POST error:', error);
+            alert('unable to add pet');
+        });
+    };// end POST route
     
 
 });// end PetsController
--- server/routers/pets.router.js
+++ server/routers/pets.router.js
@@ -8,6 +8,18 @@ const pool = require('../modules/pool.js');
 //DELETE route
 
 //POST route
+router.post('/', function(req, res){
+    const petToAdd = req.body;
+    console.log ('In pet POST route: ', petToAdd);
+    const query = `INSERT INTO "pets" ("name", "color", "breed") VALUES ($1, $2);`;
+    pool.query(query, [petToAdd.name, petToAdd.color, petToAdd.breed])
+    .then(()=>{
+        res.sendStatus(201);
+    }).catch((error)=>{
+        console.log('Error in pet POST', error);
+        res.sendStatus(500);
+    });
+});// End POST route
 
 //PUT route
 
