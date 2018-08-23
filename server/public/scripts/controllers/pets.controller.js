274644585ffbeaab72679af6716cf808e1bfb3e7
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-23 14:29:27 -0500

    server side and client side DELETE written

--- server/public/scripts/controllers/pets.controller.js
+++ server/public/scripts/controllers/pets.controller.js
@@ -1,38 +1,54 @@
-myApp.controller('PetsController', function($http){
+myApp.controller('PetsController', function ($http) {
     const vm = this;
     console.log('NG in pets');
     vm.pets = [];
 
     //GET route to pull pets from the pets table within the database
-    function getPets(){
+    function getPets() {
         console.log('in getPets');
-       $http({
-           method: 'GET',
-           url: '/pets'
-       }).then(function(response){
-           console.log('/pets GET success:', response.data);
-           vm.pets = response.data;
-       }).catch(function(error){
-           console.log('/pets GET error:', error);
-           alert('unable to get pets');
-       })
-   }
-   
+        $http({
+            method: 'GET',
+            url: '/pets'
+        }).then(function (response) {
+            console.log('/pets GET success:', response.data);
+            vm.pets = response.data;
+        }).catch(function (error) {
+            console.log('/pets GET error:', error);
+            alert('unable to get pets');
+        });
+    };// end GET route
+
     //POST route to add pets to pets table within the database
-    vm.addToPets = function(petToAdd) {
+    vm.addToPets = function (petToAdd) {
         console.log('in addToPets');
         $http({
             method: 'POST',
             url: '/pets',
             data: petToAdd
-        }).then(function(response){
+        }).then(function (response) {
             console.log('/pets POST success:', response);
-        }).catch(function (error){
+            getPets();
+        }).catch(function (error) {
             console.log('/pets POST error:', error);
             alert('unable to add pet');
         });
     };// end POST route
-    
 
-getPets();    
+    //DELETE route to remove a pet from the pets table within the database
+    vm.deletePet = function(petToDelete) {
+        console.log('in deletePet');
+        $http({
+            method: 'DELETE',
+            url: '/pets',
+            data: petToDelete
+        }).then(function(response){
+            console.log('/pets DELETE success:', response);
+            getPets();
+        }).catch(function(error){
+            console.log('/pets DELETE error:', error);
+            alert('unable to delete pet');
+        });
+    };//end DELETE route
+
+    getPets();
 });// end PetsController
--- server/routers/pets.router.js
+++ server/routers/pets.router.js
@@ -4,31 +4,45 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 //GET route to pull pet data from the pets table within the database
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     console.log('in pet GET route');
     const query = 'SELECT * FROM "pets";';
-    pool.query(query).then((results)=>{
+    pool.query(query).then((results) => {
         console.log('pets GET results', results.rows);
         res.send(results.rows);
-    }).catch((error)=>{
+    }).catch((error) => {
         console.log('Error in pet GET', error);
         res.sendStatus(500);
     });
-});
-//DELETE route
+});// end GET route
+
+//DELETE route to remoeve a pet from the pets table within the database
+router.delete('/:id', function (req, res) {
+    console.log('In pet DELETE route');
+    const id = req.params.id;
+    const query = 'DELETE FROM "pets" WHERE "id" = ($1);';
+    pool.query(query, [id])
+        .then((results) => {
+            console.log(results);
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log('Error with pet DELETE', error);
+            res.sendStatus(500);
+        });
+});//end DELETE route
 
 //POST route to add pet data to the pets table within the database
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
     const petToAdd = req.body;
-    console.log ('In pet POST route: ', req.body);
+    console.log('In pet POST route: ', req.body);
     const query = `INSERT INTO "pets" ("name", "color", "breed") VALUES ($1, $2, $3);`;
     pool.query(query, [petToAdd.name, petToAdd.color, petToAdd.breed])
-    .then(()=>{
-        res.sendStatus(201);
-    }).catch((error)=>{
-        console.log('Error in pet POST', error);
-        res.sendStatus(500);
-    });
+        .then(() => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('Error in pet POST', error);
+            res.sendStatus(500);
+        });
 });// End POST route
 
 //PUT route
