57ecc2f77b68a39794b82d5259d65f25ce35fcbe
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-23 14:49:14 -0500

    server side owner GET written

---
+++ server/routers/owners.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../modules/pool.js');
+
+//GET
+router.get('/', function(req,res){
+    console.log('in owner GET route');
+    const query = 'SELECT * FROM "owners";';
+    pool.query(query).then((results)=>{
+        console.log('owners GET results', results.rows);
+    }).catch((error)=>{
+        console.log('Error in owner GET', error);
+        res.sendStatus(500);
+    });
+});
+
+
+//POST
+
+//DELETE
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const petsRouter = require('./routers/pets.router.js');
+const ownersRouter = require('./routers/owners.router.js');
 
 //Configure body-parser
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 //Setup Routes
 app.use('/pets', petsRouter);
+app.use('/owners', ownersRouter);
 
 //Static Files
 app.use(express.static('server/public'));
