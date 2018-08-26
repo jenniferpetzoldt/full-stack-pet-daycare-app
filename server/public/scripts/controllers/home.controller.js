ffd0fee61380e94580874c0581577635dccb9b82
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-26 15:24:24 -0500

    material cards and pet form

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -2,8 +2,4 @@ myApp.controller('HomeController', function($http){
     const vm = this;
     console.log('NG in home');
 
-vm.goto = function(page) {
-    vm.status = "goto" + page;
-};
-
 });// end HomeController
